4635ec3b017fbb2e8afd442eee7f493c2abeb7dc
Fathionsons <gathefathe29@gmail.com>
2024-11-27 22:56:27 +0100

    Improving the Spinner component
    Improving the Spinner component

--- Frontend/src/components/ui/spinner.tsx
+++ Frontend/src/components/ui/spinner.tsx
@@ -1,7 +1,6 @@
-
 const Spinner: React.FC = () => (
   <div className="flex justify-center items-center space-x-2">
-    <div className="w-4 h-4 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+    <div className="w-4 h-4 border-4 border-t-transparent border-white rounded-full animate-spin"></div>
   </div>
 );
 
